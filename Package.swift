dd6b028b1e834f40017c86abded448073a465e02
Roman Pshenichnikov <pshenichnikov.r@gmail.com>
2025-12-18 12:40:04 +0300

    chore: fix Package.swift and add Fluent dependencies

--- Package.swift
+++ Package.swift
@@ -3,15 +3,38 @@ import PackageDescription
 
 let package = Package(
     name: "Trainer-bot",
-    platforms: [.macOS(.v13)],
-    products: [.executable(name: "Run", targets: ["Run"])],
+    platforms: [
+        .macOS(.v13)
+    ],
+    products: [
+        .executable(name: "Run", targets: ["Run"]),
+        .library(name: "App", targets: ["App"])
+    ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.90.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.92.0"),
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.9.0"),
+        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0")
     ],
     targets: [
-        .target(name: "App", dependencies: [
-            .product(name: "Vapor", package: "vapor")
-        ], path: "Sources/App"),
-        .executableTarget(name: "Run", dependencies: ["App"], path: "Run")
+        .target(
+            name: "App",
+            dependencies: [
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "Fluent", package: "fluent"),
+                .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver")
+            ],
+            path: "Sources/App",
+            resources: [
+                .copy("Resources")
+            ]
+        ),
+        .executableTarget(
+            name: "Run",
+            dependencies: [
+                .target(name: "App"),
+                .product(name: "Vapor", package: "vapor")
+            ],
+            path: "Run"
+        )
     ]
 )
